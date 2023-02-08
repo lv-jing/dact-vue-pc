bf88335da683ce19748cc0761fe3c03cd0be8b89
18717001370 <lvjing520cxc>
2023-02-08 17:35:23 +0800

    提交

--- src/views/ProjectProcess/SecurityImplementation.vue
+++ src/views/ProjectProcess/SecurityImplementation.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div>
     <p><a href="">安全实施有关流程说明</a></p>
@@ -12,16 +11,17 @@
       @row-click="rowClick"
       :header-cell-style="{background:'#f0f9eb', color:'black'}"
       style="width: 100%">
-      <el-table-column prop="enterprise" label="评审文档名称"></el-table-column>
-      <el-table-column prop="enterprise_short" label="Version"></el-table-column>
-      <el-table-column prop="vehicle_name" label="文档作者"></el-table-column>
-      <el-table-column prop="address" label="评审人员"></el-table-column>
-      <el-table-column prop="display_name" label="评审时间"></el-table-column>
-      <el-table-column prop="display_name" label="评审意见"></el-table-column>
-      <el-table-column prop="display_name" label="评审确认"></el-table-column>
-      <el-table-column prop="display_name" label="批准人"></el-table-column>
-      <el-table-column prop="display_name" label="批准意见"></el-table-column>
-      <el-table-column prop="display_name" label="批准时间"></el-table-column>
+      <el-table-column prop="name" label="评审文档名称"></el-table-column>
+      <el-table-column prop="version" label="Version"></el-table-column>
+      <el-table-column prop="author" label="文档作者"></el-table-column>
+      <el-table-column prop="personnel" label="评审人员"></el-table-column>
+      <el-table-column prop="opinion" label="评审意见"></el-table-column>
+      <el-table-column prop="status" label="评审状态"></el-table-column>
+      <el-table-column prop="time" label="评审时间"></el-table-column>
+      <el-table-column prop="approver" label="批准人"></el-table-column>
+      <el-table-column prop="comments" label="批准意见"></el-table-column>
+      <el-table-column prop="approvalsStatus" label="批准状态"></el-table-column>
+      <el-table-column prop="approvalsTime" label="批准时间"></el-table-column>
       <el-table-column label="评审文档链接">
         <template>
           <el-button
@@ -45,52 +45,80 @@
       <el-form :model="formData" ref="formData" :rules="rules" label-width="110px" class="demo-ruleForm">
         <el-row :gutter="24">
           <el-col :span="12">
-            <el-form-item label="评审文档名称" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="评审文档名称" prop="name">
+              <el-input v-model.number="formData.name" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="Version" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="Version" prop="version">
+              <el-input v-model.number="formData.version" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="文档作者" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="文档作者" prop="author">
+              <el-input v-model.number="formData.author" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="评审人员" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="评审人员" prop="personnel">
+              <el-input v-model.number="formData.personnel" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="评审时间" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="评审时间" prop="time">
+              <el-input v-model.number="formData.time" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="评审意见" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="评审意见" prop="opinion">
+              <el-input v-model.number="formData.opinion" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="批准人" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="批准人" prop="approver">
+              <el-input v-model.number="formData.approver" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="批准意见" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="批准意见" prop="comments">
+              <el-input v-model.number="formData.comments" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="批准时间" prop="age">
-              <el-input v-model.number="formData.age" autocomplete="off"></el-input>
+            <el-form-item label="批准时间" prop="approvalsTime">
+              <el-input v-model.number="formData.approvalsTime" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="24">
-            <el-form-item label="评审文档">
+            <el-form-item label="待评审文档">
+              <el-upload
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :before-remove="beforeRemove"
+                multiple
+                :file-list="fileList">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="已评审文档">
+              <el-upload
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :before-remove="beforeRemove"
+                multiple
+                :file-list="fileList">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="已修订文档">
               <el-upload
                 class="upload-demo"
                 action="https://jsonplaceholder.typicode.com/posts/"
@@ -117,7 +145,6 @@
 <script>
 
 export default {
-  name: "SecurityImplementation",
   props: ['activeName'],
   data() {
     return {
@@ -131,26 +158,60 @@ export default {
       },
       tableData: [
         {
-          enterprise: '车辆系统网络安全TARA',
-          enterprise_short: 'SAIC',
-          vehicle_name: 'EV52',
-          address: '上海市杨浦区军工路2500号',
-          display_name: '陈泓睿',
-          created_at: '2022-11-17 23:46:57',
-        }, {
-          enterprise: '车辆系统网络安全TARA',
-          enterprise_short: 'SAIC',
-          vehicle_name: 'EV52',
-          address: '上海市杨浦区军工路2500号',
-          display_name: '陈泓睿',
-          created_at: '2022-11-17 23:46:57',
+          name:'TARA报告',
+          version:'1.0',
+          author:'网络安全开发工程师',
+          personnel:'系统设计工程师',
+          opinion:'修改xxx',
+          status:'同意',
+          time:'2023-2-1',
+          approver:'高级经理',
+          comments:'修改xxx',
+          approvalsStatus:'退回',
+          approvalsTime:'2023-2-1',
+        },
+        {
+          name:'TARA报告',
+          version:'1.1',
+          author:'网络安全开发工程师',
+          personnel:'系统设计工程师',
+          opinion:'修改xxx',
+          status:'同意',
+          time:'2023-2-1',
+          approver:'高级经理',
+          comments:'修改xxx',
+          approvalsStatus:'退回',
+          approvalsTime:'2023-2-1',
+        },
+        {
+          name:'TARA报告',
+          version:'1.2',
+          author:'网络安全开发工程师',
+          personnel:'系统设计工程师',
+          opinion:'修改xxx',
+          status:'同意',
+          time:'2023-2-1',
+          approver:'高级经理',
+          comments:'修改xxx',
+          approvalsStatus:'退回',
+          approvalsTime:'2023-2-1',
         }
       ],
       formData:{
-        age:'',
+        name:'',
+        version:'',
+        author:'',
+        personnel:'',
+        opinion:'',
+        status:'',
+        time:'',
+        approver:'',
+        comments:'',
+        approvalsStatus:'',
+        approvalsTime:'',
       },
       rules:{
-        age: [
+        name: [
           {required: true, message: '请输入', trigger: 'change'},
         ],
       },
@@ -159,9 +220,16 @@ export default {
   },
   methods: {
     // 点击行数据事件
-    rowClick(row, column, event){
+    rowClick(row){
+      this.resetForm('formData')
+      if(row.starttime && row.endtime){
+        row.time = [row.starttime,row.endtime]
+      }
+      this.formData ={
+        ...this.formData,
+        ...row
+      }
       this.visible = true
-      console.log(row, column, event);
     },
     onSubmit() {
       console.log('submit!');
--- src/views/ProjectProcess/SecurityPlanning.vue
+++ src/views/ProjectProcess/SecurityPlanning.vue
@@ -1,106 +1,106 @@
 <template>
-    <div>
-      <el-button @click="handleAdd" style="margin-bottom: 15px" type="primary" icon="el-icon-plus"
-                 size="mini"></el-button>
-      <el-table
-          :data="tableData"
-          :key="activeName"
-          highlight-current-row
-          @row-click="rowClick"
-          :header-cell-style="{background:'#f0f9eb', color:'black'}"
-          style="width: 100%">
-          <el-table-column prop="enterprise" label="流程域"></el-table-column>
-          <el-table-column prop="enterprise_short" label="项目活动"></el-table-column>
-          <el-table-column prop="vehicle_name" label="活动类型"></el-table-column>
-          <el-table-column prop="address" label="输出物"></el-table-column>
-          <el-table-column prop="display_name" label="启动时间"></el-table-column>
-          <el-table-column prop="display_name" label="结束时间"></el-table-column>
-          <el-table-column prop="display_name" label="负责人"></el-table-column>
-          <el-table-column prop="display_name" label="参与者"></el-table-column>
-          <el-table-column prop="display_name" label="评审人"></el-table-column>
-          <el-table-column prop="display_name" label="批准人"></el-table-column>
-        </el-table>
-        <pagination
-          v-show="total>0"
-          :total="total"
-          :page.sync="queryParams.pageNum"
-          :limit.sync="queryParams.pageSize"
-          @pagination="getList"
-        />
-      <!-- 编辑新增弹窗   -->
-      <el-dialog
-        :visible.sync="dialogVisible"
-        width="45%"
-        :before-close="handleClose">
-        <el-form :model="formData" ref="formData" :rules="rules" label-width="110px" class="demo-ruleForm">
-          <el-row :gutter="24">
-            <el-col :span="12">
-              <el-form-item label="流程域" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="项目活动" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="活动类型" prop="age">
-                <el-select class="w100" v-model="formData.type" clearable placeholder="请选择">
-                  <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="输出物" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24">
-              <el-form-item label="启止时间" prop="age">
-                <el-date-picker
-                  style="width: 100%"
-                  v-model="formData.time"
-                  type="daterange"
-                  range-separator="至"
-                  start-placeholder="开始日期"
-                  end-placeholder="结束日期">
-                </el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="负责人" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="参与者" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="评审人" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="批准人" prop="age">
-                <el-input v-model.number="formData.age" autocomplete="off"></el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-form>
-        <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">取 消</el-button>
+  <div>
+    <el-button @click="handleAdd" style="margin-bottom: 15px" type="primary" icon="el-icon-plus"
+               size="mini"></el-button>
+    <el-table
+      :data="tableData"
+      :key="activeName"
+      highlight-current-row
+      @row-click="rowClick"
+      :header-cell-style="{background:'#f0f9eb', color:'black'}"
+      style="width: 100%">
+      <el-table-column prop="process" label="流程域"></el-table-column>
+      <el-table-column prop="activities" label="项目活动"></el-table-column>
+      <el-table-column prop="type" label="活动类型"></el-table-column>
+      <el-table-column prop="output" label="输出物"></el-table-column>
+      <el-table-column prop="starttime" label="启动时间"></el-table-column>
+      <el-table-column prop="endtime" label="结束时间"></el-table-column>
+      <el-table-column prop="person" label="负责人"></el-table-column>
+      <el-table-column prop="participant" label="参与者"></el-table-column>
+      <el-table-column prop="reviewer" label="评审人"></el-table-column>
+      <el-table-column prop="approver" label="批准人"></el-table-column>
+    </el-table>
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+    <!-- 编辑新增弹窗   -->
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="50%">
+      <el-form :model="formData" ref="formData" :rules="rules" label-width="110px" class="demo-ruleForm">
+        <el-row :gutter="24">
+          <el-col :span="12">
+            <el-form-item label="流程域" prop="process">
+              <el-input v-model.number="formData.process" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="项目活动" prop="activities">
+              <el-input v-model.number="formData.activities" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="活动类型" prop="type">
+              <el-select class="w100" v-model="formData.type" clearable placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="输出物" prop="output">
+              <el-input v-model.number="formData.output" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="启止时间" prop="time">
+              <el-date-picker
+                value-format="yyyy-MM-dd"
+                style="width: 100%"
+                v-model="formData.time"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="负责人" prop="person">
+              <el-input v-model="formData.person" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="参与者" prop="participant">
+              <el-input v-model.number="formData.participant" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="评审人" prop="reviewer">
+              <el-input v-model.number="formData.reviewer" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="批准人" prop="approver">
+              <el-input v-model.number="formData.approver" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="handleClose">取 消</el-button>
     <el-button type="primary" @click="submitForm('formData')">确 定</el-button>
   </span>
-      </el-dialog>
-    </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -119,50 +119,86 @@ export default {
       },
       tableData: [
         {
-          enterprise:'上汽乘用车',
-          enterprise_short:'SAIC',
-          vehicle_name:'EV52',
-          address:'上海市杨浦区军工路2500号',
-          display_name:'陈泓睿',
-          created_at:'2022-11-17 23:46:57',
-        },{
-          enterprise:'上汽乘用车',
-          enterprise_short:'SAIC',
-          vehicle_name:'EV52',
-          address:'上海市杨浦区军工路2500号',
-          display_name:'陈泓睿',
-          created_at:'2022-11-17 23:46:57',
+          id: 1,
+          process: '整车网络安全分析设计',
+          activities: '整车TARA分析',
+          type: '安全分析（TARA）',
+          output: '《TARA报告》《TARA核查报告》',
+          starttime: '2023-2-1',
+          endtime: '2023-2-20',
+          person: '网络安全开发工程师',
+          participant: '网络安全验证工程师、架构分析工程师、系统设计工程师',
+          reviewer: '架构分析工程师、系统设计工程师',
+          approver: 'Xxx高级经理'
+        }, {
+          id: 2,
+          process: '整车网络安全分析设计',
+          activities: '整车概念设计',
+          type: '安全概念（concept）',
+          output: '《安全概念报告》《概念核查报告》',
+          starttime: '2023-2-1',
+          endtime: '2023-2-20',
+          person: '网络安全开发工程师',
+          participant: '网络安全验证工程师、架构分析工程师、系统设计工程师',
+          reviewer: '系统设计工程师',
+          approver: 'Xxx高级经理'
         }
       ],
       dialogVisible: false,
-      formData:{},
-      rules:{
-        pass: [
-          { required: true, message: '请输入', trigger: 'change' },
+      formData: {
+        process: '',
+        activities: '',
+        type: '',
+        output: '',
+        starttime: '',
+        endtime: '',
+        person: '',
+        participant: '',
+        reviewer: '',
+        approver: ''
+      },
+      rules: {
+        process: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        activities: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        type: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        output: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        person: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        participant: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        reviewer: [
+          {required: true, message: '请输入', trigger: 'change'},
+        ],
+        approver: [
+          {required: true, message: '请输入', trigger: 'change'},
         ],
       },
       options: [{
-        value: '选项1',
-        label: '黄金糕'
+        value: '安全分析',
+        label: '安全分析'
       }, {
-        value: '选项2',
-        label: '双皮奶'
+        value: '安全概念',
+        label: '安全概念'
       }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
+        value: '安全设计',
+        label: '安全设计'
       }],
     };
   },
   watch: {
     '$route': { // $route可以用引号，也可以不用引号
-      handler(to){
-        if(to.query.active==='first'){
+      handler(to) {
+        if (to.query.active === 'first') {
           this.initData()
         }
       },
@@ -170,35 +206,50 @@ export default {
       immediate: true, // 第一次初始化渲染就可以监听到
     }
   },
-  mounted() {
-    console.log(this.activeName);
-  },
   methods: {
     // 初始化数据
-    initData(){
+    initData() {
       console.log('请求页面数据');
     },
     // 点击行数据事件
-    rowClick(row, column, event){
+    rowClick(row) {
+      this.resetForm('formData')
+      if(row.starttime && row.endtime){
+        row.time = [row.starttime,row.endtime]
+      }
+      this.formData ={
+        ...this.formData,
+        ...row
+      }
       this.dialogVisible = true
-      this.title = 'Edit projectSpace'
-      console.log(row, column, event);
     },
     // 获取分页数据
     getList() {
       console.log(this.queryParams);
     },
     // 添加
-    handleAdd(){
+    handleAdd() {
       this.dialogVisible = true
+      this.resetForm('formData')
+
     },
     // y提交表单
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('submit!');
+          let arr = JSON.parse(JSON.stringify(this.tableData));
+          if(this.formData.time.length>0){
+            this.formData.starttime = this.formData.time[0]
+            this.formData.endtime = this.formData.time[1]
+          }
+          arr.unshift(this.formData)
+          let hash = {}
+          this.tableData = arr.reduce((item, next) => {
+            hash[next.id] ? '' : hash[next.id] = true && item.push(next)
+            return item
+          }, [])
+          this.dialogVisible = false
         } else {
-          console.log('error submit!!');
           return false;
         }
       });
